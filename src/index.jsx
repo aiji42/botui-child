7f3c48ada697f7d895861599ea3b552d39c956d5
Aiji Uejima <aiji42@gmail.com>
2020-03-08 19:54:02 +0900

    parent側で起動・非起動を決めるためいらない処理を削除

--- src/index.jsx
+++ src/index.jsx
@@ -3,25 +3,14 @@ import ReactDOM from 'react-dom';
 import ChatField from './components/ChatField';
 import { handshake } from './handshake';
 
-import { saveStoreValue, findStoredValue } from './dataStore';
-
 window.dataLayer = window.dataLayer || [];
-
-const shouldStartChat = ({ activateRate }) => {
-  if (typeof activateRate !== 'number') return true;
-  return saveStoreValue('activeate', findStoredValue('activeate', Math.random() <= activateRate));
-};
 
 const main = async () => {
   const parent = await handshake;
 
   ReactDOM.render(<ChatField />, document.getElementById('root'));
 
-  window.dataLayer.push({
-    event: 'analytics', eventCategory: 'botui-child', eventAction: 'activate',
-    eventLabel: shouldStartChat(parent.model) ? 'true' : 'false'
-  });
-  shouldStartChat(parent.model) ? parent.emit('readyToStartChat') : parent.emit('doNotStartChat');
+  parent.emit('readyToStartChat');
 };
 
 main();
