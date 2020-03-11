4f6ad03433c0077a4e3223a0ea245ecd37f894ca
AijiUejima <aiji42@gmail.com>
2020-03-11 09:54:25 +0900

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -3,15 +3,16 @@ import ReactDOM from 'react-dom';
 import ChatField from './components/ChatField';
 import { handshake } from './handshake';
 
+window.dataLayer = window.dataLayer || [];
+
 (async () => {
   const parent = await handshake;
 
-  ReactDOM.render(<ChatField />, document.getElementById('root'));
-
-  window.dataLayer = window.dataLayer || [];
-  window.dataLayer.push({
-    stage: process.env.NODE_ENV,
-    serviceCode: parent.model.setting.serviceCode
+  ReactDOM.render(<ChatField />, document.getElementById('root'), () => {
+    window.dataLayer.push({
+      stage: process.env.NODE_ENV,
+      serviceCode: parent.model.setting.serviceCode
+    });
+    parent.emit('readyToStartChat');
   });
-  parent.emit('readyToStartChat');
 })();
